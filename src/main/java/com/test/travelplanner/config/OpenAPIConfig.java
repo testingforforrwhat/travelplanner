8c5089113d62bfcb3a19d66b522d6deb7720c93c
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-11-16 13:49:42 +0800

    // http://localhost:8080/swagger-ui.html

--- src/main/java/com/test/travelplanner/config/OpenAPIConfig.java
+++ src/main/java/com/test/travelplanner/config/OpenAPIConfig.java
@@ -1,9 +1,11 @@
 package com.test.travelplanner.config;
 
-import io.swagger.v3.oas.models.OpenAPI;  
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;  
-import io.swagger.v3.oas.models.info.Contact;  
-import org.springframework.context.annotation.Bean;  
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -13,7 +15,13 @@ public class OpenAPIConfig {
     
     @Bean  
     public OpenAPI customOpenAPI() {  
-        return new OpenAPI()  
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
                 .info(new Info()  
                         .title("Travel Planner API")
                         .version("1.0")  
