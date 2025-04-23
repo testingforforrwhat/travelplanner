746bb2140a2279d677d7f6a4010399715dbed658
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-04-24 02:47:15 +0800

    fix(WebSocketConfig.java):

--- src/main/java/com/test/travelplanner/config/WebSocketConfig.java
+++ src/main/java/com/test/travelplanner/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.test.travelplanner.config;
 
 import com.test.travelplanner.websocket.ChatWebSocketHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -15,4 +17,10 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry.addHandler(new ChatWebSocketHandler(), "/chat")
                 .setAllowedOrigins("*"); // 允许跨域
     }
+
+    @Bean
+    public WebSocketHandler chatWebSocketHandler() {
+        return new ChatWebSocketHandler();
+    }
+
 }
