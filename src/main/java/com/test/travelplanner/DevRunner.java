77855c014dac08ecff99f14bffcf7bb4d19575f3
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-05-18 20:04:41 +0800

    ci(DevRunner.java): add init data

--- src/main/java/com/test/travelplanner/DevRunner.java
+++ src/main/java/com/test/travelplanner/DevRunner.java
@@ -1,11 +1,16 @@
 package com.test.travelplanner;
 
 
+import com.test.travelplanner.model.entity.DestinationEntity;
+import com.test.travelplanner.repository.DestinationRepository;
+import com.test.travelplanner.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Component
@@ -14,11 +19,16 @@ public class DevRunner implements ApplicationRunner {
 
     static private final Logger logger = LoggerFactory.getLogger(DevRunner.class);
 
+    private final DestinationRepository destinationRepository;
+
 
     public DevRunner(
 
+            DestinationRepository destinationRepository
+
     ) {
 
+        this.destinationRepository = destinationRepository;
     }
 
 
@@ -31,6 +41,11 @@ public class DevRunner implements ApplicationRunner {
 
 
     private void generateSampleData() {
+
+        destinationRepository.saveAll(List.of(
+              new DestinationEntity("洛杉矶","洛杉矶","阳光明媚，电影之都的魅力","@/assets/images/sydney.jpg",2.0),
+              new DestinationEntity("洛杉矶","洛杉矶","阳光明媚，电影之都的魅力","@/assets/images/sydney.jpg",2.0)
+        ));
 
     }
 }
