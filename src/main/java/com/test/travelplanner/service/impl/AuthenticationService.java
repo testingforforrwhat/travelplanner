8fcbfcdc96772d1067eb02b3fb55a74b1dc5e735
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-05-23 20:04:12 +0800

    Update AuthenticationService.java

--- src/main/java/com/test/travelplanner/service/impl/AuthenticationService.java
+++ src/main/java/com/test/travelplanner/service/impl/AuthenticationService.java
@@ -4,6 +4,7 @@ package com.test.travelplanner.service.impl;
 import com.alibaba.fastjson2.JSON;
 import com.test.travelplanner.authentication.UserAlreadyExistException;
 import com.test.travelplanner.model.dto.user.LoginAuthResponse;
+import com.test.travelplanner.model.dto.user.UserDTO;
 import com.test.travelplanner.model.entity.UserEntity;
 import com.test.travelplanner.model.entity.UserRole;
 import com.test.travelplanner.redis.RedisUtil;
@@ -64,8 +65,13 @@ public class AuthenticationService {
 
        UserEntity userEntity = userRepository.findByUsername(username);
 
+       UserDTO userDTO = new UserDTO();
+       userDTO.setUserId(userEntity.getId());
+       userDTO.setName(userEntity.getUsername());
+       userDTO.setEmail(userEntity.getEmail());
+
        // ( spring cloud ) 签发的令牌，存入Redis中。拼接上Authorization的策略（Bearer Token）前缀。
-       redisUtil.set( "Bearer " + token , userEntity , 60 * 24 );
+       redisUtil.set( "Bearer " + token , userDTO , 60 * 24 );
 
        authResponse.setUser(username);
 
